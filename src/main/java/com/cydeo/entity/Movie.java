bf535a671d76fea858380591a05185f20577c466
raduga9199 <raduga9199@gmail.com>
2023-07-12 10:00:20 -0400

    CinemaLab

--- src/main/java/com/cydeo/entity/Movie.java
+++ src/main/java/com/cydeo/entity/Movie.java
@@ -2,15 +2,13 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.MovieState;
 import com.cydeo.enums.MovieType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -18,21 +16,21 @@ import java.time.LocalDate;
 public class Movie extends BaseEntity{
 
     private String name;
-
     @Column(columnDefinition = "DATE")
     private LocalDate releaseDate;
-
     private Integer duration;
-
     @Column(columnDefinition = "text")
     private String summary;
-
     @Enumerated(EnumType.STRING)
     private MovieType type;
-
     @Enumerated(EnumType.STRING)
     private MovieState state;
-
     private BigDecimal price;
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList;
 
 }
