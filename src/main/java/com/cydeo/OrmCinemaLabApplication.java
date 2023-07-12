b227c64f2c65bedf135357622a12455185060625
raduga9199 <raduga9199@gmail.com>
2023-07-12 10:31:46 -0400

    Flyway -Migrate

--- src/main/java/com/cydeo/OrmCinemaLabApplication.java
+++ src/main/java/com/cydeo/OrmCinemaLabApplication.java
@@ -1,7 +1,12 @@
 package com.cydeo;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class OrmCinemaLabApplication {
@@ -10,4 +15,8 @@ public class OrmCinemaLabApplication {
         SpringApplication.run(OrmCinemaLabApplication.class, args);
     }
 
+    @Bean // Bean has to be created before migration (Not used in real environment)
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
 }
